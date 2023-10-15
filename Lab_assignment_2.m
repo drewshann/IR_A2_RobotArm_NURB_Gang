06f81aa546df3278b86c270ebda48ff674414bd2
Andrew Shannon <andrew.j.shannon@student.uts.edu.au>
2023-10-15 15:10:27 +1100

    UR3 moves to multiple goals - The UR3 also now has an initial guess for the ikcon function - Also added templates for collision checks and singularities

--- Lab_assignment_2.m
+++ Lab_assignment_2.m
@@ -3,11 +3,22 @@ classdef Lab_assignment_2 < handle
     properties
         rob1;
         rob2;
-
+        current_item;
+        items_pos;
     end
 
     properties(Constant)
-        
+        initialGuessQRob1 = [-0.7849   -1.7363    0.0001    0.1327   -1.6231    0.7849];
+
+        apple_pos1 = [deg2rad(43.2), deg2rad(-43.2), deg2rad(-14.4), deg2rad(28.8), deg2rad(43.2), deg2rad(28.8)];  % From teach
+        test_single_pos = transl(0.4,0,0.2) * trotx(-pi);
+
+        test_multiple_pos = {transl(0.4,0,0.3)*trotx(-pi), ...
+                            transl(-0.4,0,0.3)*trotx(-pi), ...
+                            transl(0.4,0.1,0.3)*trotx(-pi), ...
+                            transl(-0.4,0.1,0.3)*trotx(-pi), ...
+                            transl(-0.2,0.2,0.5)*trotx(-pi), ...
+                            transl(0,0,0.6)*trotx(-pi)};
     end
 
     methods
@@ -19,6 +30,8 @@ classdef Lab_assignment_2 < handle
                 self.rob2 = robot2;     
 
             else
+                self.rob1 = UR3;
+                self.items_pos = self.test_multiple_pos;
 
             end
 
@@ -33,7 +46,7 @@ classdef Lab_assignment_2 < handle
 
             % Run fkine after ikcon to test whether we got the correct end
             % effector position
-            q1 = self.rob1.model.ikcon(transform);
+            q1 = self.rob1.model.ikcon(transform, self.initialGuessQRob1);
         end
 
         function q2 = transformation2Q_rob2(self, transform)
@@ -58,17 +71,65 @@ classdef Lab_assignment_2 < handle
             % UR3 = UR3_control.robot;
             
             r1Traj = jtraj(start_q_rob1,end_q_rob1,trsteps);
-            r2Traj = jtraj(start_q_rob2,end_q_rob2,trsteps);
+            % r2Traj = jtraj(start_q_rob2,end_q_rob2,trsteps);
 
             for i = 1:trsteps
                 % r2.model.fkine(UR3.model.getpos)
                 self.rob1.model.animate(r1Traj(i,:));
-                self.rob2.model.animate(r2Traj(i,:));
+                % self.rob2.model.animate(r2Traj(i,:));
                 % self.update_gripper_pos();
 
                 drawnow()       
             end
             
+        end
+
+%% Function used to begin picking up apples autonomously
+        function pickApples(self)
+            num_items = size(self.test_multiple_pos);
+% Idea for how to have both robots picking up apples at the same time:
+% Have one big matrix for the positions of the apples and in that matrix
+% also encode a boolean which tells whether it is currently being picked or
+% not. So the cell array would look like this:
+%   [transform for item position, true;
+%   transform for item position, true;
+%   transform for item position, false;
+%   transform for item position, false]
+
+            for i = 1:num_items(1,2)
+                self.current_item = i;
+               
+
+                % Get current position and the q at that position
+                qPos1 = self.rob1.model.getpos();
+
+                % Gripper positioned z + 0.09 of brick.
+                gripper_pos = self.items_pos{i};
+                gripper_pos(3,4) = gripper_pos(3,4)+0.09;
+                % Calc q at the end position
+                qPos2 = self.transformation2Q_rob1(gripper_pos);
+                disp(["The current brick is number: ",i]);
+                disp("The current brick being picked up has the transform: ");
+                disp(self.items_pos{i});
+                
+                % Open gripper and transform between current position and
+                % starting brick position
+                % self.open_gripper();
+                self.transform(qPos1,qPos2,qPos1,qPos1);
+
+
+            end
+        end
+
+%% Function used to check for collisions and return a true/false if the robot will collide with anything, including the other robot
+        function collision = checkCollisions(self)
+            
+            
+        end
+
+%% Singularity Detection function and returns a cell array of the singularity locations
+        function singularity = singularityLocations(self)
+
         end
 
 
--- Lab_assignment_2_main.m
+++ Lab_assignment_2_main.m
@@ -2,4 +2,13 @@ clc
 clear all
 close all
 
-obj = Lab_assignment_2()
+
+
+obj = Lab_assignment_2();
+
+% obj.rob1.model.teach();
+% test_pos_1 = obj.rob1.model.getpos();
+% test_pos_2 = obj.transformation2Q_rob1(obj.test_pos);
+% obj.transform(test_pos_1, test_pos_2, test_pos_2, test_pos_2);
+
+obj.pickApples()
