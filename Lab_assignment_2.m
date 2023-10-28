75d7cb61d1fc5a7afbecf2a2950a0d2b19bf36f6
Andrew Shannon <andrew.j.shannon@student.uts.edu.au>
2023-10-28 18:12:54 +1100

    Changed robot to ur5, fixed the collision checking function with ur5 and added basic rmrc control

--- Lab_assignment_2.m
+++ Lab_assignment_2.m
@@ -7,6 +7,7 @@ classdef Lab_assignment_2 < handle
         items_pos;
         cube;
         robot;
+        Truck;
     end
 
     properties(Constant)
@@ -30,6 +31,15 @@ classdef Lab_assignment_2 < handle
                         [0.05,0.05,0.08535], ...
                         [0.05,0.05,0.0819], ...
                         [0.05,0.05,0.05]};
+
+        ur5_link_sizes = {[0.05,0.05,0.089159], ...
+                        [-0.425,0.05,0.05], ...
+                        [-0.39225,0.05,0.05], ...
+                        [0.05,0.05,0.10915], ...
+                        [0.05,0.05,0.09465], ...
+                        [0.05,0.05,0.0823], ...
+                        [0.05,0.05,0.05]};   
+
         
         % % Cell array for the links of the UR3 with q as all zeros
         % UR3_links = {eye(4), ...                                                        % Base transform
@@ -51,13 +61,13 @@ classdef Lab_assignment_2 < handle
                 self.rob2 = robot2;     
 
             else
-                self.rob1 = UR3;
+                self.rob1 = UR5;
                 self.items_pos = self.test_multiple_pos;
 
             end
 
             hold on
-            self.cube = PlaceObject("BasicObstacle.ply",[5.0,0,0]);
+            % self.cube = PlaceObject("BasicObstacle.ply",[5.0,0,0]);
             self.setup_environment();
         end
 
@@ -100,6 +110,7 @@ classdef Lab_assignment_2 < handle
                 self.rob1.model.animate(r1Traj(i,:));
                 % self.rob2.model.animate(r2Traj(i,:));
                 % self.update_gripper_pos();
+                self.checkCollisions();
 
                 drawnow()
             end
@@ -108,42 +119,89 @@ classdef Lab_assignment_2 < handle
 
         %% Robotic control via Resolved Motion Rate Control
         function rmrc(self, start_q_rob1, end_q_rob1, start_q_rob2, end_q_rob2)
-            deltaT = 0.05;
-            x1 = [start_q_rob1]';
-            x2 = [end_q_rob1]';
+            t = 10;             % Total time (s)
+            deltaT = 0.05;      % Control frequency
+            steps = t/deltaT;   % No. of steps for simulation
+            delta = 2*pi/steps; % Small angle change
+            epsilon = 0.1;      % Threshold value for manipulability/Damped Least Squares
+            W = diag([1 1 1 0.1 0.1 0.1]);    % Weighting matrix for the velocity vector
 
-            % Assign memory and interpolate two points from x1 to x2
-            x = zeros(self.rob1.model.n,self.trsteps);
-            s = lspb(0,1,self.trsteps); 
-            for i = 1:self.trsteps
-                x(:,i) = x1*(1-s(i)) + s(i)*x2;
+            % 1.2) Allocate array data
+            m = zeros(steps,1);             % Array for Measure of Manipulability
+            qMatrix = zeros(steps,6);       % Array for joint anglesR
+            qdot = zeros(steps,6);          % Array for joint velocities
+            theta = zeros(3,steps);         % Array for roll-pitch-yaw angles
+            x = zeros(3,steps);             % Array for x-y-z trajectory
+            positionError = zeros(3,steps); % For plotting trajectory error
+            angleError = zeros(3,steps);    % For plotting trajectory error
+
+            % 1.3) Set up trajectory, initial pose
+            s = lspb(0,1,steps);                % Trapezoidal trajectory scalar
+            for i=1:steps
+                x(1,i) = (1-s(i))*0.35 + s(i)*0.35; % Points in x
+                x(2,i) = (1-s(i))*0 + s(i)*0.2; % Points in y
+                x(3,i) = 0.25;% + 0.2*sin(i*delta); % Points in z
+                theta(1,i) = 0;                 % Roll angle 
+                theta(2,i) = 5*pi/9;            % Pitch angle
+                theta(3,i) = 0;                 % Yaw angle
             end
+            
+            % Transform becomes imaginary here
+            T1 = transl(x(1,1),x(1,2),x(1,3))*troty(theta(2,1));
+            T = [rpy2r(theta(1,1),theta(2,1),theta(3,1)) x(:,1);zeros(1,3) 1];          % Create transformation of first point and angle
+            q0 = zeros(1,6);                                                            % Initial guess for joint angles
+            qMatrix(1,:) = self.rob1.model.ikcon(T1,q0);                                            % Solve joint angles to achieve first waypoint
 
 
-            qMatrix = nan(self.trsteps,self.rob1.model.n);
-            end_eff = self.rob1.model.fkine(self.rob1.model.getpos);
-            qMatrix(1,:) = self.transformation2Q_rob1(end_eff);
-
-            for i = 1:self.trsteps-1
-                % Calculate velocity at discrete time step
-                xdot = (x(:,i+1) - x(:,i))/deltaT;
-                % Get the Jacobian at the current state
-                J = self.rob1.model.jacob0(qMatrix(i,:));
-                % Take only first 6 rows
-                J = J(self.rob1.model.n,:);
-                qdot = inv(J)*xdot;   % Solve velocitities via RMRC
-                qMatrix(i+1,:) =  qMatrix(i,:) + deltaT*qdot';  % Update next joint state
-
+            % 1.4) Track the trajectory with RMRC
+            for i = 1:steps-1
+                % UPDATE: fkine function now returns an SE3 object. To obtain the 
+                % Transform Matrix, access the variable in the object 'T' with '.T'.
+                T = self.rob1.model.fkine(qMatrix(i,:)).T;                                           % Get forward transformation at current joint state
+                deltaX = x(:,i+1) - T(1:3,4);                                         	% Get position error from next waypoint
+                Rd = rpy2r(theta(1,i+1),theta(2,i+1),theta(3,i+1));                     % Get next RPY angles, convert to rotation matrix
+                Ra = T(1:3,1:3);                                                        % Current end-effector rotation matrix
+                Rdot = (1/deltaT)*(Rd - Ra);                                                % Calculate rotation matrix error
+                S = Rdot*Ra';                                                            % Skew symmetric!
+                linear_velocity = (1/deltaT)*deltaX;
+                angular_velocity = [S(3,2);S(1,3);S(2,1)];                              % Check the structure of Skew Symmetric matrix!!
+                % deltaTheta = tr2rpy(Rd*Ra');                                            % Convert rotation matrix to RPY angles
+                xdot = W*[linear_velocity;angular_velocity];                          	% Calculate end-effector velocity to reach next waypoint.
+                J = self.rob1.model.jacob0(qMatrix(i,:));                 % Get Jacobian at current joint state
+                m(i) = sqrt(det(J*J'));
+                if m(i) < epsilon  % If manipulability is less than given threshold
+                    lambda = (1 - m(i)/epsilon)*5E-2;
+                else
+                    lambda = 0;
+                end
+                invJ = inv(J'*J + lambda *eye(6))*J';                                   % DLS Inverse
+                qdot(i,:) = (invJ*xdot)';                                                % Solve the RMRC equation (you may need to transpose the         vector)
+                for j = 1:6                                                             % Loop through joints 1 to 6
+                    if qMatrix(i,j) + deltaT*qdot(i,j) < self.rob1.model.qlim(j,1)                     % If next joint angle is lower than joint limit...
+                        qdot(i,j) = 0; % Stop the motor
+                    elseif qMatrix(i,j) + deltaT*qdot(i,j) > self.rob1.model.qlim(j,2)                 % If next joint angle is greater than joint limit ...
+                        qdot(i,j) = 0; % Stop the motor
+                    end
+                end
+                qMatrix(i+1,:) = qMatrix(i,:) + deltaT*qdot(i,:);                         	% Update next joint state based on joint velocities
+                positionError(:,i) = x(:,i+1) - T(1:3,4);                               % For plotting
+                % angleError(:,i) = deltaTheta;                                           % For plotting
             end
-
-            for i = 1:self.trsteps
+            
+            % 1.5) Plot the results
+            tic
+            
+            % plot3(x(1,:),x(2,:),x(3,:),'k.','LineWidth',1)
+            for i = 1:steps
                 self.rob1.model.animate(qMatrix(i,:));
-                % hold on
-                % end_eff = self.rob1.model.fkine(qMatrix(i,:));
-                % end_eff = end_eff.T;
-                % plot3(end_eff(1,4),end_eff(2,4),end_eff(3,4),'r.');
+                hold on
+                end_eff = self.rob1.model.fkine(qMatrix(i,:));
+                end_eff = end_eff.T;
+                plot3(end_eff(1,4),end_eff(2,4),end_eff(3,4),'r.');
                 drawnow();
             end
+            
+            disp(['Plot took ', num2str(toc), 'seconds'])
         end
 
         %% Function used to begin picking up apples autonomously
@@ -191,12 +249,13 @@ classdef Lab_assignment_2 < handle
             transforms = cell(1,7);
             midpoints = cell(1,7);
             NumberOfLinks = size(self.rob1.model.links);
+            disp(["Number of links on the robot: ", NumberOfLinks]);
             current_robq = self.rob1.model.getpos;
 
             % Initial ellipsoid centerpoint
 
             for i = 1:NumberOfLinks(1,2)+1
-                [X,Y,Z] = ellipsoid(0,0,0,self.ur3_link_sizes{i}(1),self.ur3_link_sizes{i}(2),self.ur3_link_sizes{i}(3));
+                [X,Y,Z] = ellipsoid(0,0,0,self.ur5_link_sizes{i}(1),self.ur5_link_sizes{i}(2),self.ur5_link_sizes{i}(3));
                 EllipsoidPoints = [X(:),Y(:),Z(:)];
 
                 transforms{i} = self.manual_fkine_rob1(current_robq,i);
@@ -237,7 +296,7 @@ classdef Lab_assignment_2 < handle
                     disp((transforms{i}(1:3,4) + transforms{i-1}(1:3,4))/2);
                 end
 
-
+                hold on
                 EllipsoidPointsAndOnes = [midpoints{i} * [EllipsoidPoints,ones(size(EllipsoidPoints,1),1)]']';
                 updatedEllipsoidPoints = EllipsoidPointsAndOnes(:,1:3);
                 plot3(updatedEllipsoidPoints(:,1), EllipsoidPointsAndOnes(:,2), EllipsoidPointsAndOnes(:,3));
@@ -245,11 +304,11 @@ classdef Lab_assignment_2 < handle
             
             
 
-            % verts = [get(self.cube,'Vertices'), ones(size(get(self.cube,'Vertices'),1), 1)];
+            % verts = [get(self.Truck,'Vertices'), ones(size(get(self.Truck,'Vertices'),1), 1)];
             % verts(:,4) = [];
             % disp(verts);
             % 
-            % cubeAtOigin_h = plot3(verts(:,1),verts(:,2),verts(:,3),'r.');
+            % % cubeAtOigin_h = plot3(verts(:,1),verts(:,2),verts(:,3),'r.');
             % 
             % for i = 1:NumberOfLinks(1,2)+1
             %     d = ((verts(:,1)-midpoints{i}(1,4))/self.ur3_link_sizes{i}(1)).^2 ...
@@ -292,16 +351,16 @@ classdef Lab_assignment_2 < handle
         function Transform = manual_fkine_rob1(self,q,return_link)
 
             if return_link > 7  % 6 degrees of freedom and a tool transform
-                disp("The transform for the link number cannot be returned as it exceeds the UR3 robot's links");
+                disp("The transform for the link number cannot be returned as it exceeds the UR5 robot's links");
                 return
             end
             linksArray = {self.rob1.model.base, ...
-                        trotz(q(1)) * transl(0,0,0.1519) * transl(0,0,0) * trotx(pi/2), ...
-                        trotz(q(2)) * transl(0,0,0) * transl(-0.24365,0,0) * trotx(0), ...
-                        trotz(q(3)) * transl(0,0,0) * transl(-0.21325,0,0) * trotx(0), ...
-                        trotz(q(4)) * transl(0,0,0.11235) * transl(0,0,0) * trotx(pi/2), ...
-                        trotz(q(5)) * transl(0,0,0.08535) * transl(0,0,0) * trotx(-pi/2), ...
-                        trotz(q(6)) * transl(0,0,0.0819) * transl(0,0,0) * trotx(0), ...
+                        trotz(q(1)) * transl(0,0,0.089159) * transl(0,0,0) * trotx(pi/2), ...
+                        trotz(q(2)) * transl(0,0,0) * transl(-0.425,0,0) * trotx(0), ...
+                        trotz(q(3)) * transl(0,0,0) * transl(-0.39225,0,0) * trotx(0), ...
+                        trotz(q(4)) * transl(0,0,0.10915) * transl(0,0,0) * trotx(pi/2), ...
+                        trotz(q(5)) * transl(0,0,0.09465) * transl(0,0,0) * trotx(-pi/2), ...
+                        trotz(q(6)) * transl(0,0,0.0823) * transl(0,0,0) * trotx(0), ...
                         eye(4)};
             % linksArray = {base_tr, joint1To2, joint2To3, joint3To4, joint4To5, joint5To6, joint6To7, tool_tr};
             
@@ -316,12 +375,12 @@ classdef Lab_assignment_2 < handle
 
 
         function plot_robot_no_ply(self)
-            L1 = Link('d',0.1519,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
-            L2 = Link('d',0,'a',-0.24365,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
-            L3 = Link('d',0,'a',-0.21325,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
-            L4 = Link('d',0.11235,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
-            L5 = Link('d',0.08535,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
-            L6 = Link('d',0.0819,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
+            L1 = Link('d',0.089159,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            L2 = Link('d',0,'a',-0.425,'alpha',0,'offset',0,'qlim',[deg2rad(-90),deg2rad(90)]);
+            L3 = Link('d',0,'a',-0.39225,'alpha',0,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
+            L4 = Link('d',0.10915,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            L5 = Link('d',0.09465,'a',0,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            L6 = Link('d',0.0823,'a',0,'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);   
              
             self.robot = SerialLink([L1,L2,L3,L4,L5,L6],'name','random_robot');
 
@@ -331,7 +390,7 @@ classdef Lab_assignment_2 < handle
     %% Environment Setup
         function setup_environment(self)
             hold on
-            Truck = PlaceObject('NewTruckPlatform.ply',[0 0 0]);
+            self.Truck = PlaceObject('NewTruckPlatform.ply',[0 0 0]);
             Apple1 = PlaceObject('NewApple.ply',[0 1.2 1]);
             Apple2 = PlaceObject('NewApple.ply',[1 1.2 1.2]);
             Apple3 = PlaceObject('NewApple.ply',[1.2 1.2 1.4]);
--- Lab_assignment_2_main.m
+++ Lab_assignment_2_main.m
@@ -10,7 +10,21 @@ obj = Lab_assignment_2();
 % test_pos_1 = obj.rob1.model.getpos();
 % test_pos_2 = obj.transformation2Q_rob1(obj.test_pos);
 % obj.transform(test_pos_1, test_pos_2, test_pos_2, test_pos_2);
+start_q = obj.rob1.model.getpos();
+final_pos = transl(0,0,1);
+final_q = obj.transformation2Q_rob1(final_pos);
+disp("Does it get to here?");
+obj.rmrc(start_q,final_q,start_q,start_q);
+
+obj.checkCollisions();
+disp("WHAT IS HAPPENING");
 
 
 
-obj.pickApples()
+% obj.pickApples()
+% start_q = obj.rob1.model.getpos();
+% final_pos = transl(0,0,1);
+% final_q = obj.transformation2Q_rob1(final_pos);
+% disp("Does it get to here?");
+
+obj.transform_interpolation(start_q,final_q,start_q,start_q);
