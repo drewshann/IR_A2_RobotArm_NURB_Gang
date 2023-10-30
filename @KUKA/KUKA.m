d12317472c3703827c91bfe879dcefc81aa5f599
Christiaan Marais <christiaan.c.marais@student.uts.edu.au>
2023-10-30 22:55:40 +1100

    Vertice reduction on workspace & KUKA PLY loading

--- @KUKA/KUKA.m
+++ @KUKA/KUKA.m
@@ -21,11 +21,11 @@ function self = KUKA(baseTr)
 %% CreateModel
         function CreateModel(self)
             %link(1) = Link([pi     0       0       pi/2    1]); % PRISMATIC Link
-            link(1) = Link('d',0.1519,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
-            link(2) = Link('d',0,'a',-0.24365,'alpha',0,'qlim', deg2rad([-180 1]), 'offset',0);
-            link(3) = Link('d',0,'a',-0.21325,'alpha',0,'qlim', deg2rad([-180 180]), 'offset', 0);
-            link(4) = Link('d',0.11235,'a',0,'alpha',pi/2,'qlim',deg2rad([-180 180]),'offset', 0);
-            link(5) = Link('d',0.08535,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+            link(1) = Link('d',0.1519,'a',0,'alpha',pi/2,'qlim',deg2rad([-166 166]), 'offset',0);
+            link(2) = Link('d',0,'a',-0.24365,'alpha',0,'qlim', deg2rad([-188 45]), 'offset',0);
+            link(3) = Link('d',0,'a',-0.21325,'alpha',0,'qlim', deg2rad([-116 153]), 'offset', 0);
+            link(4) = Link('d',0.11235,'a',0,'alpha',pi/2,'qlim',deg2rad([-185 185]),'offset', 0);
+            link(5) = Link('d',0.08535,'a',0,'alpha',-pi/2,'qlim',deg2rad([-110,110]), 'offset',0);
 
              
             %link(1).qlim = [-0.8, -0.01];
